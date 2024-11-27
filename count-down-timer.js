24288174a0970e329965338f996c50e133c7fd0a
Elisheva Klor <elishevaklor@gmail.com>
2024-11-27 16:49:08 +0200

    initial project

---
+++ count-down-timer.js
@@ -0,0 +1,17 @@
+
+let countdown = 10;
+
+function startCountdown() {
+    let timer = setInterval(() => {
+        console.log(`נשארו ${countdown} שניות...`);
+
+        if (countdown <= 0) {
+            clearInterval(timer); 
+            console.log("הטיימר נגמר! 🎉");
+        } else {
+            countdown--;
+        }
+    }, 1000); 
+}
+
+startCountdown();
